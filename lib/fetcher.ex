b6baca086be6e099d56c9b17b5fbc713fb2f3e51
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-10-31 16:46:00 +0530

    wrap up

--- lib/fetcher.ex
+++ lib/fetcher.ex
@@ -6,7 +6,10 @@ defmodule Fetcher do
   # TODO extract the parallel worker in a macro?
 
   def fetch(receiver_pid, url) do
-    get url
+    IO.puts "FETCHING #{url} in #{inspect self}"
+    {microseconds, response} = :timer.tc fn -> get(url) end
+    IO.puts "FETCHED #{url} in #{microseconds / 1000}ms"
+    send(receiver_pid, {:done, self, response})
   end
 
 end
--- lib/parallel.ex
+++ lib/parallel.ex
@@ -1,11 +1,11 @@
 defmodule Parallel do
-  @timeout 5000
+  @timeout_ms 3000
 
   def run do
-
     # read the urls
-    urls = File.read!("./urls") |> String.split("\n")
-    IO.inspect(urls |> hd)
+    urls = File.read!("./urls")
+           |> String.split("\n")
+           |> Enum.reject(fn line -> line == "" end)
 
     # spawn as many processes as the number of urls
     # with the receiver as self
@@ -15,22 +15,45 @@ defmodule Parallel do
     end)
 
     # wait for all the processes to finish
-    wait(pids_and_refs, @timeout)
+    # schedule a terminate message in timeout
+    Process.send_after(self, :timeout, @timeout_ms)
+    wait(pids_and_refs)
   end
 
-  defp wait(pids_and_refs, timeout) do
-    # schedule a terminate message in timeout
-    Process.send_after(self, :timeout, timeout)
-
+  defp wait([]), do: :ok
+  defp wait(pids_and_refs) do
     # receive messages and reduce the pid and refs
     receive do
-      {:done, data} ->
-        :ok
-      :terminate ->
+      {:done, worker_pid, data} ->
+        process(data) # should this also be async?
+        # TODO: consolidate the two loops
+        # demonitor the ref
+        pids_and_refs
+        |> Enum.find(fn({pid, ref}) -> pid == worker_pid end)
+        |> (fn({_pid, ref}) -> Process.demonitor(ref, [:flush]) end).()
+
+        pids_and_refs
+        |> Enum.reject(fn({pid, _ref}) -> pid == worker_pid end)
+        |> wait
+      :timeout ->
         terminate_stragglers(pids_and_refs)
+      {:DOWN, worker_ref, :process, _pid, _reason} ->
+        # this process crashed, we are ignoring these guys for now
+        pids_and_refs
+        |> Enum.reject(fn({_pid, ref}) -> ref == worker_ref end)
+        |> wait
     end
   end
 
   defp terminate_stragglers(pids_and_refs) do
+    Enum.each(pids_and_refs, fn({pid, ref}) ->
+      Process.demonitor(ref, [:flush])
+      Process.exit(pid, :kill)
+      IO.puts "killed #{inspect pid} due to timeout"
+    end)
+  end
+
+  defp process(data) do
+    IO.puts "DONE" # " with #{inspect data}"
   end
 end
